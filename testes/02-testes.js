65229b581d8fd8007a3497f86a57e91c3a8d57ee
Andre Henrique Miqueleto Nicolau <amiqueletonicola@apple.com>
2025-06-21 16:23:44 -0300

    ci: novos ajustes

---
+++ testes/02-testes.js
@@ -0,0 +1,28 @@
+import { exibirNomeDogFormatado } from '../conceitos/testes-de-unidade.js'
+import assert, { fail } from 'node:assert'
+
+describe('Testes do Projeto', () => {
+  it(`deve exibir o nome do dog com letras maiúsculas`, () => {
+    assert.strictEqual(exibirNomeDogFormatado('Fred'), 'FRED')
+    
+    // verifique se: <valor atual> É IGUAL A <valor esperado>
+  })
+
+  it('primeiro teste', () => {
+
+  })
+
+  it('segundo teste', async function() {
+    // throw new Error()    
+    assert.equal(exibirNomeDogFormatado('ToBiaS'), 'TOBIAS');
+  })
+
+  it('Forçando Erro!', () => {
+    try {
+      throw new Error('Forçando um erro')  
+    } catch (error) {
+      assert.fail(`Erro esperado, apenas para testar o erro: ${error.message}`)
+    }
+  })
+
+})
